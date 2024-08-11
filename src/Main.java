0d5f2b7fcb3ada3c94b1c4bc5560084828271cb2
w-kon <wkon@yandex.ru>
2024-08-11 20:56:40 +0600

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int score = 430;
-        int bonus_price = 100;
-        int topping_up = 2200;
-        int bonus_score = topping_up / bonus_price;
-        System.out.println("Итого баланс " + (score + topping_up + bonus_score) + " руб.");
-        System.out.println("Бонусов зачислено " + bonus_score + " руб.");
+        int bonusPrice = 100;
+        int toppingUp = 2200;
+        int bonusScore = toppingUp / bonusPrice;
+        System.out.println("Итого баланс " + (score + toppingUp + bonusScore) + " руб.");
+        System.out.println("Бонусов зачислено " + bonusScore + " руб.");
 
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
